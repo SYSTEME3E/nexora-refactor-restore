c85a86274e2dfa77c8f0d4aa1935c9fce1326e4e
SYSTEME3E <communautemiprec@gmail.com>
2026-03-29 00:14:22 +0100

    Update ChatWidget.tsx

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -5,7 +5,7 @@ import { getNexoraUser } from "@/lib/nexora-auth";
 
 // Photo d'assistante féminine via URL publique (Unsplash)
 const ASSISTANT_PHOTO =
-  "https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=80&h=80&fit=crop&face";
+  "https://i.postimg.cc/y8ZYR0gq/file-00000000b90c7246ab59b08eaba09eb0.png";
 
 function formatTime(iso: string) {
   return new Date(iso).toLocaleTimeString("fr-FR", {
